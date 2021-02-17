239c30f9c1adb65c9d3b47326e8be182880f4e8c
Isaac <isaac@valr.com>
2021-02-17 10:08:17 +0200

    adjust header for mobile layout

--- src/pages/homePage/home.js
+++ src/pages/homePage/home.js
@@ -5,12 +5,15 @@ import WeatherDetails from "../../components/weatherDetails";
 import Footer from "../../components/footer";
 
 import { Main, Container } from "./styles";
+import { Desktop } from "../../styles";
 
 const HomePage = () => (
   <Container>
     <Main>
       <Header />
-      <WeatherDetails />
+      <Desktop>
+        <WeatherDetails />
+      </Desktop>
     </Main>
     <Footer />
   </Container>
