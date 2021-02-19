267516c5e0b71bf52a2254bdd2d3779d7a755724
Isaac <isaac@valr.com>
2021-02-19 06:17:40 +0200

    use current day of week to get active forcast tile

--- src/helpers/date.js
+++ src/helpers/date.js
@@ -1,3 +1,27 @@
 import moment from "moment";
 
-export const dateNow = moment().format("DD MMM YYYY");
+const momentNow = moment();
+export const dateNow = momentNow.format("DD MMM YYYY");
+
+export function getWeekDay() {
+  const weekDay = momentNow.day();
+
+  switch (weekDay) {
+    case 0:
+      return "Sun";
+    case 1:
+      return "Mon";
+    case 2:
+      return "Tue";
+    case 3:
+      return "Wed";
+    case 4:
+      return "Thu";
+    case 5:
+      return "Fri";
+    case 6:
+      return "Sat";
+    default:
+      return "Mon";
+  }
+}
