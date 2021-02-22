3cc59d3fa36438fd71dc335cb7ed73e9cee1307e
Isaac <isaac@valr.com>
2021-02-22 11:49:59 +0200

    fix .env empty variables

--- src/api/location.js
+++ src/api/location.js
@@ -1,10 +1,10 @@
 import { userLocationUrl, userLocationApiKey } from "../helpers";
 
 export const locationConfig = {
-  url: "https://api.ipdata.co",
+  url: userLocationUrl,
   method: "get",
   params: {
-    "api-key": "a8e267211fbfbe622685cbb24901614fc8e5901b0c870353df3aa8d6",
+    "api-key": userLocationApiKey,
   },
   headers: { Accept: "application/json" },
 };
